d889db9dc4ca332498ad2b222e8d44b71ba091a0
Matt <jtag@syncsrc.org>
2018-06-10 17:38:29 -0700

    added toolchain to build command line

--- build_efi.sh
+++ build_efi.sh
@@ -5,7 +5,6 @@
 
 make -C edk2/BaseTools
 cd edk2/
-sed -i "s/TOOL_CHAIN_TAG[[:space:]]*=.*$/TOOL_CHAIN_TAG = GCC5/" Conf/target.txt
 . edksetup.sh
-build -a X64 -p ShellPkg/ShellPkg.dsc -b RELEASE
-build -a X64 -p Uload/Uload.dsc -b RELEASE
+build -a X64 -p ShellPkg/ShellPkg.dsc -b RELEASE -t GCC5
+build -a X64 -p Uload/Uload.dsc -b RELEASE -t GCC5
