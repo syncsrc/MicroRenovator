50c53402069c3466a0017607c8e9b90f9cb9a82c
Matt <jtag@syncsrc.org>
2018-08-04 14:51:22 -0700

    remove startup delay

--- build_efi.sh
+++ build_efi.sh
@@ -3,6 +3,7 @@
 ## A script to build Uload.efi and dependancies
 ## Requires https://github.com/tianocore/edk2
 
+sed -i 's/ShellInfoObject.ShellInitSettings.BitUnion.Bits.NoInterrupt  = FALSE/ShellInfoObject.ShellInitSettings.BitUnion.Bits.NoInterrupt  = TRUE/' edk2/ShellPkg/Application/Shell/Shell.c
 make -C edk2/BaseTools
 cd edk2/
 . edksetup.sh
